518f607c765758b82c6567799d321e44c244d276
Manuel Valenzuela <Manuel_Valenzuela@jabil.com>
2023-06-30 07:27:50 -0600

    added event store

--- CQRS-ES/CQRS.Core/Events/EventModel.cs
+++ CQRS-ES/CQRS.Core/Events/EventModel.cs
@@ -8,7 +8,7 @@ public class EventModel
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
-    public DateTime TimeStamp { get; set; }+
+    public DateTime TimeStamp { get; set; }
     public Guid AggregateIdentifier { get; set; }
     public string AggregateType { get; set; }
     public int Version { get; set; }
