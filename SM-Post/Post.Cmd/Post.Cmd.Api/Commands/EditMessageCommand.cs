fe8dffd07cadc2fd353a1b8ca6cda3e4efef0e91
Manuel Valenzuela <Manuel_Valenzuela@jabil.com>
2023-06-30 08:56:50 -0600

    added commando handler

--- SM-Post/Post.Cmd/Post.Cmd.Api/Commands/EditMessageCommand.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/EditMessageCommand.cs
@@ -2,7 +2,7 @@ using CQRS.Core.Commands;
 
 namespace Post.Cmd.Api.Commands;
 
-public class EdirMessageCommand : BaseCommand
+public class EditMessageCommand : BaseCommand
 {
     public required string Message { get; set; }
 }
