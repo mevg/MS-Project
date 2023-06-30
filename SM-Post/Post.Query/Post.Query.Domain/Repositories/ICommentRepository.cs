50bccbc19ad5845e3baa45317eb2778e2618ff65
Manuel Valenzuela <Manuel_Valenzuela@jabil.com>
2023-06-30 14:50:19 -0600

    comment repository

--- SM-Post/Post.Query/Post.Query.Domain/Repositories/ICommentRepository.cs
+++ SM-Post/Post.Query/Post.Query.Domain/Repositories/ICommentRepository.cs
@@ -4,8 +4,8 @@ namespace Post.Query.Domain.Repositories;
 
 public interface ICommentRepository
 {
-    Task CreateAsync(CommentEntity coment);
-    Task UpdateAsync(CommentEntity coment);
+    Task CreateAsync(CommentEntity comment);
+    Task UpdateAsync(CommentEntity comment);
     Task<CommentEntity> GetByIdAsync(Guid commentId);
     Task DeleteAsync(Guid commentId);
 }
